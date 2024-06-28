a80ffd8705d54b61e578c73ceb2e7d853ed32a87
FlorianMinguet <florian.minguet@epitech.eu>
2024-06-28 15:01:08 +0200

    feat(recognition): ytb videos are now playable

--- src/bg_substracting.py
+++ src/bg_substracting.py
@@ -1,51 +1,63 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
+from pytube import YouTube
 
+# Function to download YouTube video
+def download_video(url, output_path='video.mp4'):
+    yt = YouTube(url)
+    stream = yt.streams.filter(file_extension='mp4').first()
+    stream.download(filename=output_path)
+    return output_path
+
+# Argument parser setup
 parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
                                               OpenCV. You can process both videos and images.')
 parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
 parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
 args = parser.parse_args()
 
-## [create]
-#create Background Subtractor objects
+# Create Background Subtractor objects
 if args.algo == 'MOG2':
     backSub = cv.createBackgroundSubtractorMOG2()
 else:
     backSub = cv.createBackgroundSubtractorKNN()
-## [create]
 
-## [capture]
-capture = cv.VideoCapture(0)
+# URL of the video to process
+url = 'https://www.youtube.com/watch?v=Qaod9E2P9Zk'
+
+# Download the video
+video_path = download_video(url)
+
+# Open the downloaded video
+capture = cv.VideoCapture(video_path)
 if not capture.isOpened():
-    print('Unable to open: ' + args.input)
+    print('Unable to open: ' + video_path)
     exit(0)
-## [capture]
 
+# Process the video frames
 while True:
     ret, frame = capture.read()
     if frame is None:
         break
 
-    ## [apply]
-    #update the background model
+    # Update the background model
     fgMask = backSub.apply(frame)
-    ## [apply]
 
-    ## [display_frame_number]
-    #get the frame number and write it on the current frame
-    cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    # Display frame number on the current frame
+    cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
     cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-    ## [display_frame_number]
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-    ## [show]
-    #show the current frame and the fg masks
+    # Show the current frame and the foreground mask
     cv.imshow('Frame', frame)
     cv.imshow('FG Mask', fgMask)
-    ## [show]
 
+    # Break the loop if 'q' or 'ESC' is pressed
     keyboard = cv.waitKey(30)
-    if keyboard == 'q' or keyboard == 27:
+    if keyboard == ord('q') or keyboard == 27:
         break
+
+# Release the video capture object and close all OpenCV windows
+capture.release()
+cv.destroyAllWindows()
